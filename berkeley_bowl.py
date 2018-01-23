59515167d2ab9e41c12073d65008d03723bc40a2
Austin Davis-Richardson <harekrishna@gmail.com>
2018-01-22 16:57:23 -0800

    refactored

---
+++ berkeley_bowl.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+
+def location_soups():
+    '''
+    Scrape soups list of Berkeley Bowl's webpage.
+    '''
+
+    url = 'http://www.berkeleybowl.com/daily-hot-soup'
+
+    res = requests.get(url)
+
+    soup = BeautifulSoup(res.content, 'html5lib')
+
+    locations = soup.find_all('div', class_='block-inner')[3:5]
+
+    for location in locations:
+
+        location_name = location.find(class_='block-title').text
+
+        time_string = location.find_all(class_='field-item')[0].text
+        timestamp = datetime.strptime(time_string, '%A, %B %d, %Y - %I:%M')
+
+        # parse timestamp
+
+        soups = [ n.text for n in location.find_all(class_='field-item')[1:] ]
+
+        yield {
+            'name': location_name,
+            'timestamp': timestamp,
+            'soups': soups
+            }
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Berkeley Bowl Soup Scraper
+
+Scraping soups from the Berkeley Bowl webpage and writing to an Sqlite
+database
+
+Sqlite database looks like this:
+
+- `timestamp`
+- `location_name`
+- `soups` (comma-separated list of soups)
